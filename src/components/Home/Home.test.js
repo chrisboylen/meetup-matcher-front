704605accef3b055f601d880d57049ae5500dff6
Chris Boylen <billpolster@gmail.com>
2018-09-10 13:34:44 -0600

    Update test and pass test Home component should match snapshot

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import Home from './Home';
+import { shallow, render, mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import { Home } from './Home';
+import { CitySearch } from '../../containers/CitySearch/CitySearch';
 
 describe('Home', () => {
   it('should match snapshot', () => {
     const wrapper = shallow(<Home />);
-
+      
     expect(wrapper).toMatchSnapshot();
   });
 });
