4b037937917ad65a149f191788a6999c7aab99f1
Chris Boylen <billpolster@gmail.com>
2018-09-10 15:34:53 -0600

    Add and pass test CitySearch handleChange method
    Should set state when invoked

--- src/containers/CitySearch/CitySearch.test.js
+++ src/containers/CitySearch/CitySearch.test.js
@@ -3,9 +3,29 @@ import { shallow } from 'enzyme';
 import { CitySearch } from './CitySearch';
 
 describe('CitySearch', () => {
+  let wrapper;
+  let mockEvent;
+
+  beforeEach(() => {
+    mockEvent = {target: {value: 'bingo'}};
+    wrapper = shallow(<CitySearch />);
+  });
+
   it('should match snapshot', () => {
     const wrapper = shallow(<CitySearch />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleChange', () => {
+    it('should set state when invoked', () => {
+      mockEvent = {target: {name: 'city', value: 'bingo'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().city).toBe('bingo');
+
+      mockEvent = {target: {name: 'state', value: 'bongo'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().state).toBe('bongo');
+    });
   });
 });
