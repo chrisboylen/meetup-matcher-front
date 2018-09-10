cd7eb40cc8d15a73614aa7bb6d15dabaf1b2aa33
Chris Boylen <billpolster@gmail.com>
2018-09-10 10:29:21 -0600

    Add and pass test App.test.js
    Should match the snapshot

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,4 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
+describe('App', () => {
+  it('should match snapshot', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper).toMatchSnapshot()
+  })
+});
