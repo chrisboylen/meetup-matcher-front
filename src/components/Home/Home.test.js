3910f4e4119e934c75113573161f546812aefb13
Chris Boylen <billpolster@gmail.com>
2018-09-10 10:34:41 -0600

    Add and pass test Home.test
    Should match snpashot

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import App from './App';
 import { shallow } from 'enzyme';
+import Home from './Home';
 
-describe('App', () => {
+describe('Home', () => {
   it('should match snapshot', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Home />);
 
     expect(wrapper).toMatchSnapshot();
   });
