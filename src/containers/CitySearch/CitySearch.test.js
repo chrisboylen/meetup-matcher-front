6680c10d9595c43a557309f178faaa3b9bf8b674
Chris Boylen <billpolster@gmail.com>
2018-09-10 16:21:30 -0600

    Add and pass test CitySearch handleSubmit method
    Should invoke handleSubmit when form is submitted

--- src/containers/CitySearch/CitySearch.test.js
+++ src/containers/CitySearch/CitySearch.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { CitySearch } from './CitySearch';
+import { displayMeetups } from '../../actions';
 
 describe('CitySearch', () => {
   let wrapper;
@@ -38,6 +39,27 @@ describe('CitySearch', () => {
       const spy = spyOn(wrapper.instance(), 'handleChange');
       wrapper.instance().forceUpdate();
       wrapper.find('.state-input').simulate('change', mockEvent);
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('handleSubmit', () => {
+    let mockDisplayMeetups;
+
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({json: () => Promise.resolve({name: 'bingo'})}));
+
+      mockDisplayMeetups = jest.fn();
+      mockEvent = {preventDefault: jest.fn()};
+      wrapper = shallow(
+        <CitySearch displayMeetups={mockDisplayMeetups} />
+      );
+    });
+    it('should invoke handleSubmit when form is submitted', () => {
+      const spy = spyOn(wrapper.instance(), 'handleSubmit');
+      wrapper.instance().forceUpdate();
+      wrapper.find('button').simulate('click', mockEvent);
 
       expect(spy).toHaveBeenCalled();
     });
