1e05db293dc5bc8ca93c7774e2496b22540213da
Chris Boylen <billpolster@gmail.com>
2018-09-12 11:46:39 -0600

    Add handleChange method to Login component

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { NavLink, withRouter } from 'react-router-dom';
+import { signInFirebase } from '../../firebase/auth';
+import { userError, loginUser } from '../../actions';
+import { auth } from '../../firebase/firebase';
+import { cleanUserInfo } from '../../Utilities/helper';
 
-export default class Login extends Component {
+export class Login extends Component {
   constructor() {
     super();
     this.state = {
@@ -9,6 +16,11 @@ export default class Login extends Component {
     }
   }
 
+  handleChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value })
+  }
   render() {
     const { email, password } = this.state;
 
