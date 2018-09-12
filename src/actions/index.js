d41145435abe40b2eca0f53f6866854b33de269e
Chris Boylen <billpolster@gmail.com>
2018-09-12 08:33:42 -0600

    Add userUrror action to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,3 +11,8 @@ export const loginUser = (user) => ({
 export const logoutUser = () => ({
   type: 'LOGOUT_USER'
 });
+
+export const userError = (message) => ({
+  type: 'USER_ERROR',
+  message
+});
