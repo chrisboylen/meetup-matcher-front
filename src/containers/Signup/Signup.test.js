64805740ebb5eacbd942ec3af857c1b660a18025
Chris Boylen <billpolster@gmail.com>
2018-09-17 17:34:34 -0600

    Add and pass test Signup.test handleChange
    Should set state when invoked

--- src/containers/Signup/Signup.test.js
+++ src/containers/Signup/Signup.test.js
@@ -4,12 +4,33 @@ import { Signup, mapStateToProps, mapDispatchToProps } from './Signup';
 
 describe('Signup Component', () => {
   let wrapper;
+  let mockEvent;
 
   beforeEach(() => {
     wrapper = shallow(<Signup />);
+    mockEvent = {target: {value: 'yes'}};
   });
 
-  it('should should match snapshot', () => {
+  it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleChange', () => {
+    it('should set state when invoked', () => {
+      mockEvent = {target: {name: 'username', value: 'tim'}};
+      wrapper.instance().handleChange(mockEvent);
+
+      expect(wrapper.state().username).toBe('tim');
+
+      mockEvent = {target: {name: 'email', value: 't@gmail.com'}};
+      wrapper.instance().handleChange(mockEvent);
+
+      expect(wrapper.state().email).toBe('t@gmail.com');
+
+      mockEvent = {target: {name: 'password', value: 'bingo'}};
+      wrapper.instance().handleChange(mockEvent);
+
+      expect(wrapper.state().password).toBe('bingo');
+    });
   });
 });
