3e9e309df7f86811e38efb9e5e47d02bc8295eee
Chris Boylen <billpolster@gmail.com>
2018-09-17 16:32:25 -0600

    Add and pass test  Questions.test handleChange
    Should set state when invoked

--- src/containers/Questions/Questions.test.js
+++ src/containers/Questions/Questions.test.js
@@ -4,12 +4,46 @@ import { Questions, mapStateToProps, mapDispatchToProps } from './Questions';
 
 describe('Questions', () => {
   let wrapper;
+  let mockEvent;
 
   beforeEach(() => {
     wrapper = shallow(<Questions />);
+    mockEvent = {target: {value: 'yes'}};
   });
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleChange', () => {
+    it('should set state when invoked', () => {
+      mockEvent = {target: {name: 'traveling'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().traveling).toBe(true);
+
+      mockEvent = {target: {name: 'entrepreneur'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().entrepreneur).toBe(true);
+
+      mockEvent = {target: {name: 'softwareDevs'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().softwareDevs).toBe(true);
+
+      mockEvent = {target: {name: 'hiking'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().hiking).toBe(true);
+
+      mockEvent = {target: {name: 'photography'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().photography).toBe(true);
+
+      mockEvent = {target: {name: 'diningout'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().diningout).toBe(true);
+
+      mockEvent = {target: {name: 'realEstate'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state().realEstate).toBe(true);
+    });
   });
 });
