7d919c6a3244619ef1696912123e3a3b1e381e08
Chris Boylen <billpolster@gmail.com>
2018-09-17 15:56:42 -0600

    Update rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { meetupsReducer } from './meetupsReducer';
 import { userReducer } from './userReducer';
 import { questionsReducer } from './questionsReducer';
+import { userErrorReducer } from './userErrorReducer';
 
 export const rootReducer = combineReducers({
   meetups: meetupsReducer,
   user: userReducer,
-  questions: questionsReducer
+  questions: questionsReducer,
+  userError: userErrorReducer
 });
