aacbd13d982996a842499bf43e5af76d20deb696
Chris Boylen <billpolster@gmail.com>
2018-09-17 12:46:06 -0600

    Add and pass test Header.test mapStateToProps
    Should return an object of user

--- src/containers/Header/Header.test.js
+++ src/containers/Header/Header.test.js
@@ -5,13 +5,29 @@ import { Header, mapStateToProps, mapDispatchToProps } from './Header';
 describe('Header Component', () => {
   let wrapper;
   let mockLogout;
+  let mockLogOutFirebase;
 
   beforeEach(() => {
     mockLogout = jest.fn();
-    wrapper = shallow(<Header logout={mockLogout} />);
+    mockLogOutFirebase = jest.fn();
+    wrapper = shallow(
+      <Header 
+        logout={mockLogout} 
+        logOutFirebase={mockLogOutFirebase}
+      />
+    );
   });
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+  describe('mapStateToProps', () => {
+    it('return an object of user', () => {
+      const mockState = {user: {name: 'Tim'}, logout: 'no'};
+      const expected = {user: {name: 'Tim'}};
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
   });
 });
