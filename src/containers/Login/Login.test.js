73d2df540f4021133c7e9963c2ad52b773570d88
Chris Boylen <billpolster@gmail.com>
2018-09-17 13:28:02 -0600

    Add and pass test Login.test handleChange
    Should set state when handleChange is invoked

--- src/containers/Login/Login.test.js
+++ src/containers/Login/Login.test.js
@@ -15,4 +15,18 @@ describe('Login Component', () => {
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('handleChange', () => {
+    it('should set state when invoked', () => {
+      mockEvent = {target: {name: 'email', value: 't@gmail.com'}};
+      wrapper.instance().handleChange(mockEvent);
+
+      expect(wrapper.state().email).toBe('t@gmail.com');
+
+      mockEvent = {target: {name: 'password', value: 'god'}};
+      wrapper.instance().handleChange(mockEvent);
+
+      expect(wrapper.state().password).toBe('god');
+    });
+  });
 });
