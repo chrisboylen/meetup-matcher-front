5666fbaf5a16ec6d946ae4d6d4ddb04e2f46a54a
Chris Boylen <billpolster@gmail.com>
2018-09-18 15:32:22 -0600

    Remove unused patch ContentRoute

--- src/containers/ContentRoute/ContentRoute.js
+++ src/containers/ContentRoute/ContentRoute.js
@@ -4,6 +4,7 @@ import { Home } from '../../components/Home/Home';
 import Login from '../Login/Login';
 import Signup from '../Signup/Signup';
 import User from '../User/User';
+import Questions from '../Questions/Questions';
 
 export const ContentRoute = () => {
   return (
@@ -12,6 +13,7 @@ export const ContentRoute = () => {
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/user" component={User} />
+      <Route exact path="/user/questions" component={Questions} />
     </Switch>
   );
 };
