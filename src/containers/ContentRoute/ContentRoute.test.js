a92dd61bbf150be1e9e1ff6f0dce7211293c37ad
Chris Boylen <billpolster@gmail.com>
2018-09-18 15:37:23 -0600

    Update ContentRoute .test snapshot

--- src/containers/ContentRoute/ContentRoute.test.js
+++ src/containers/ContentRoute/ContentRoute.test.js
@@ -1,38 +1,12 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router';
-import { shallow, render} from 'enzyme';
-import { ContentRoute, mapStateToProps, mapDispatchToProps } from './ContentRoute';
-import { displayMeetups } from '../../actions';
+import { shallow } from 'enzyme';
+import { ContentRoute } from './ContentRoute';
 
 describe('ContentRoute', () => {
   it('should match snapshot with default path', () => {
     const wrapper = shallow(<ContentRoute />);
     
     expect(wrapper).toMatchSnapshot();
-  });
-
-  describe('mapStateToProps', () => {
-    it('should return an object of meetups array', () => {
-      const mockState = {
-        meetups: [{ name: '1' }, { name: '1' }],
-        displayMeetups: 'BINGO'
-      };
-      const expected = { meetups: [{ name: '1' }, { name: '1' }] };
-      const mappedProps = mapStateToProps(mockState);
-
-      expect(mappedProps).toEqual(expected);
-    });
-  });
-
-  describe('mapDispatchToProps', () => {
-    it('dispatch displayMeetups action when displayMeetups is invoked', () => {
-      const mockDispatch = jest.fn();
-      const actionToDispatch = displayMeetups({name: '1'});
-      const mappedProps = mapDispatchToProps(mockDispatch);
-      mappedProps.displayMeetups({name: '1'});
-
-      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
-    });
   });
 });
 
