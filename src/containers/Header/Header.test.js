ebea5dd1f26324ccf0ded61ff6e465a57ad9133a
Chris Boylen <billpolster@gmail.com>
2018-09-18 13:44:19 -0600

    Update Header.test to match snapshot with current user

--- src/containers/Header/Header.test.js
+++ src/containers/Header/Header.test.js
@@ -1,43 +1,47 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Header, mapStateToProps, mapDispatchToProps } from './Header';
+import { Header, mapDispatchToProps } from './Header';
+import * as firebase from 'firebase';
 import { logoutUser } from '../../actions';
 
 describe('Header Component', () => {
   let wrapper;
   let mockLogout;
   let mockLogOutFirebase;
+  let mockHistory;
 
   beforeEach(() => {
     mockLogout = jest.fn();
     mockLogOutFirebase = jest.fn();
+    mockHistory = { push: jest.fn() }
     wrapper = shallow(
       <Header 
         logout={mockLogout} 
         logOutFirebase={mockLogOutFirebase}
+        history={mockHistory}
       />
     );
   });
 
   it('should match snapshot', () => {
+     firebase.auth.currentUser = jest.fn().mockReturnValue({
+       currentUser: true
+     });
+
     expect(wrapper).toMatchSnapshot();
   });
+  it('should match snapshot with no user', () => {
+     firebase.auth.currentUser = jest.fn().mockReturnValue({
+       currentUser: false
+     });
 
-  describe('handleLogout', () => {
-    it('should call logout when invoked', () => {
-      console.log(wrapper.debug())
-      wrapper.instance().handleLogout()
-      expect(mockLogout).toHaveBeenCalled();
-    });
+     expect(wrapper).toMatchSnapshot();
   });
+  describe('handleLogout', () => {
+    it('should call logout when invoked', async () => {
+      await wrapper.instance().handleLogout();
 
-  describe('mapStateToProps', () => {
-    it('return an object of user', () => {
-      const mockState = {user: {name: 'Tim'}, logout: 'no'};
-      const expected = {user: {name: 'Tim'}};
-      const mappedProps = mapStateToProps(mockState);
-
-      expect(mappedProps).toEqual(expected);
+      expect(mockLogout).toHaveBeenCalled();
     });
   });
 
