416dfabc8ad43b0cc16136e2fd509af0a6bc4de3
Chris Boylen <billpolster@gmail.com>
2018-09-18 17:15:50 -0600

    Fix linting errors Home

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { shallow, render, mount } from 'enzyme';
-import { MemoryRouter } from 'react-router';
+import { shallow } from 'enzyme';
 import { Home } from './Home';
-import { CitySearch } from '../../containers/CitySearch/CitySearch';
 
 describe('Home', () => {
   it('should match snapshot', () => {
